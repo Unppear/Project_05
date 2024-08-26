5d473e5f0758c3ba966a75bb0cd3f9a145484eea
Unppear <107591895+Unppear@users.noreply.github.com>
2024-08-26 13:00:43 -0300

    debug

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ function actualPage() {
         $('#contatoLink').css('color', '#eb2d2d');
         $('#contatoLink2').css('color', '#eb2d2d');
         $('html, body').animate({scrollTop: $('.contact_us').offset().top});
-    } else if(curPage == ' ') {
+    } else if(curPage[0] == ' ') {
         curPage = 'index';
 
         $('a[href=index]').css('color', '#eb2d2d');        
