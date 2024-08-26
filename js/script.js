41c80c5c5afd4ae547a9b1280174c0fa42f7631f
Unppear <107591895+Unppear@users.noreply.github.com>
2024-08-26 11:57:09 -0300

    v1.5

--- js/script.js
+++ js/script.js
@@ -42,7 +42,9 @@ function actualPage() {
         $('#contatoLink2').css('color', '#eb2d2d');
         $('html, body').animate({scrollTop: $('.contact_us').offset().top});
     } else if(curPage[0] == ' ') {
-        curPage[0] = 'index';        
+        curPage[0] = 'index';
+        
+        $('a[href=' + curPage[0] + ']').css('color', '#eb2d2d');        
     } else {
         $('a[href=' + curPage[0] + ']').css('color', '#eb2d2d');
     };
